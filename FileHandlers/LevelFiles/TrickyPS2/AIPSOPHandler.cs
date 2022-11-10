a1809a95a9c79d5d473c18857d58d1183eda3c4b
GlitcherOG <lachlankalf@gmail.com>
2022-11-10 15:08:38 +1100

    AIP/SOP & LTG Start

---
+++ FileHandlers/LevelFiles/TrickyPS2/AIPSOPHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSXMultiTool.FileHandlers.LevelFiles.TrickyPS2
+{
+    internal class AIPSOPHandler
+    {
+    }
+}
---
+++ FileHandlers/LevelFiles/TrickyPS2/LTGHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSXMultiTool.FileHandlers.LevelFiles.TrickyPS2
+{
+    internal class LTGHandler
+    {
+        byte Unknown;
+        byte ColdFusionVersion;
+        byte ColdFusionRevision;
+        byte endianess;
+
+        Vector3 WorldBounds1;
+        Vector3 WorldBounds2;
+
+        float mainBboxSize;
+        int pointerCount;
+        int pointerListCount;
+        int totalGridCount;
+        int mainBboxCount;
+        int mainBboxEmptyCount;
+
+        float nodeBoxSize;
+        int nodeBoxWidth;
+        int nodeBoxCount;
+
+        int pointerListOffset;
+        int bboxDataListOffset;
+
+    }
+}
