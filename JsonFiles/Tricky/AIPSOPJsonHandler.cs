e28efdf59db526feb52d67a58fa1d3ba4389a991
GlitcherOG <lachlankalf@gmail.com>
2022-12-04 00:13:53 +1100

    Create AIPSOPJsonHandler.cs

---
+++ JsonFiles/Tricky/AIPSOPJsonHandler.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSXMultiTool.JsonFiles.Tricky
+{
+    public class AIPSOPJsonHandler
+    {
+        public List<PathTypeAJson> PathTypeA = new List<PathTypeAJson>();
+        public List<PathTypeBJson> PathTypeB = new List<PathTypeBJson>();
+        public void CreateJson(string path)
+        {
+            var serializer = JsonConvert.SerializeObject(this, Formatting.None);
+            File.WriteAllText(path, serializer);
+        }
+
+        public static AIPSOPJsonHandler Load(string path)
+        {
+            string paths = path;
+            if (File.Exists(paths))
+            {
+                var stream = File.ReadAllText(paths);
+                var container = JsonConvert.DeserializeObject<AIPSOPJsonHandler>(stream);
+                return container;
+            }
+            else
+            {
+                return new AIPSOPJsonHandler();
+            }
+        }
+        public struct PathTypeAJson
+        {
+            public int Unknown1;
+            public int Unknown2;
+            public int Unknown3;
+            public int Unknown4;
+            public int Unknown5;
+            public int Unknown6;
+            public int Unknown7;
+
+            public float[] pathPos;
+            public float[] bboxMin;
+            public float[] bboxMax;
+
+            public List<float[]> patchPoints;
+            public List<UnkownListTypeAJson> unkownListTypeAs;
+
+        }
+
+        public struct PathTypeBJson
+        {
+            public int Unknown1;
+            public int Unknown2;
+            public int Unknown3;
+            public float Unknown4;
+
+            public float[] pathPos;
+            public float[] bboxMin;
+            public float[] bboxMax;
+
+            public List<float[]> patchPoints;
+            public List<UnkownListTypeAJson> unkownListTypeAs;
+        }
+
+        public struct UnkownListTypeAJson
+        {
+            public int Unknown1;
+            public int Unknown2;
+            public float Unknown3;
+            public float Unknown4;
+        }
+    }
+}
