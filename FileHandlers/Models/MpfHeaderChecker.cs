f536937355e2b5c2724b5d443c633c211d12b58e
GlitcherOG <lachlankalf@gmail.com>
2023-02-15 22:24:46 +1100

    MPF Checker, SSX On Tour Model Start

---
+++ FileHandlers/Models/MpfHeaderChecker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSXMultiTool.FileHandlers.Models
+{
+    public class MpfHeaderChecker
+    {
+
+        public string DetectFileType(string path)
+        {
+            //3 SSX (2000) PS2
+            //8 SSX Tricky PS2
+            //13 SSX 3 PS2
+            //xDFP SSX 3 Music
+            //14 SSX On Tour PS2
+
+
+        }
+    }
+
+}
---
+++ FileHandlers/Models/SSXOnTourMPF.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSXMultiTool.FileHandlers.Models
+{
+    public class SSXOnTourMPF
+    {
+        public byte[] magicWords = new byte[4];
+        public int NumModels;
+        public int HeaderSize;
+        public int DataOffset;
+        public void Load(string path)
+        {
+
+
+
+        }
+
+        public struct MPFHeader
+        {
+            //Main Header
+            public string ModelName;
+            public int DataOffset;
+            public int EntrySize;
+
+        }
+    }
+}
