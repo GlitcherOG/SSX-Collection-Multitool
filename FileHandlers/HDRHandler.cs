0cf7a7925a62d1dc77f2db048bb037c38fc419a0
GlitcherOG <lachlankalf@gmail.com>
2023-03-04 14:58:55 +1100

    HDR Reading Update

--- FileHandlers/HDRHandler.cs
+++ FileHandlers/HDRHandler.cs
@@ -11,12 +11,13 @@ namespace SSXMultiTool.FileHandlers
     internal class HDRHandler
     {
         public int U1;
-        int U2;
-        int U3;
-        int FileCount;
-        int PaddingCount;
-        int U5;
-        int U6;
+        public int U2;
+        public int OffsetBytes;
+        public int FileCount;
+        public int PaddingCount;
+        public int U4;
+        public int U5;
+        public int U6;
 
         List<FileHeader> fileHeaders = new List<FileHeader>();
         List<int> Padding = new List<int>();
@@ -25,26 +26,50 @@ namespace SSXMultiTool.FileHandlers
             using (Stream stream = File.Open(Path, FileMode.Open))
             {
                 U1 = StreamUtil.ReadInt16(stream);
-                U2 = StreamUtil.ReadInt16(stream);
-                U3 = StreamUtil.ReadInt8(stream);
-                FileCount = StreamUtil.ReadInt8(stream);
-                PaddingCount = StreamUtil.ReadInt16(stream);
-                U5 = StreamUtil.ReadInt24(stream);
+                U2 = StreamUtil.ReadInt16(stream); //Always -1
+                OffsetBytes = StreamUtil.ReadUInt8(stream);
+                FileCount = StreamUtil.ReadUInt8(stream);
+                PaddingCount = StreamUtil.ReadUInt8(stream);
+                U4 = StreamUtil.ReadUInt8(stream); //Multi 0 == 1
+                U5 = StreamUtil.ReadInt16(stream);
 
+                //stream
+                //StreamUtil.AlignBy16(stream);
+
+                stream.Position += 4-OffsetBytes;
 
                 fileHeaders = new List<FileHeader>();
                 for (int i = 0; i < FileCount; i++)
                 {
                     var TempHeader = new FileHeader();
-                    if(U3==3)
+                    TempHeader.Offset = StreamUtil.ReadBytes(stream, OffsetBytes);
+
+                    if(TempHeader.Offset.Length==1)
                     {
-                        TempHeader.Unknown1 = StreamUtil.ReadInt8(stream);
-                        TempHeader.Offset = StreamUtil.ReadInt16Big(stream);
-                        TempHeader.Unknown2 = StreamUtil.ReadInt16Big(stream); //Sample Rate?
+                        stream.Position -= 1;
+                        TempHeader.OffsetInt = (byte)TempHeader.Offset[0];
                     }
+                    if (TempHeader.Offset.Length == 2)
+                    {
+                        stream.Position -= 2;
+                        TempHeader.OffsetInt = StreamUtil.ReadInt16Big(stream);
+                    }
+                    if (TempHeader.Offset.Length == 3)
+                    {
+                        stream.Position -= 3;
+                        TempHeader.OffsetInt = StreamUtil.ReadInt24Big(stream);
+                    }
+                    if (TempHeader.Offset.Length == 4)
+                    {
+                        stream.Position -= 4;
+                        TempHeader.Speaker = StreamUtil.ReadUInt8(stream);
+                        TempHeader.OffsetInt = StreamUtil.ReadInt24Big(stream);
+                    }
+
+                    TempHeader.Unknown2 = StreamUtil.ReadInt16(stream);
                     fileHeaders.Add(TempHeader);
                 }
-                U6 = StreamUtil.ReadInt24(stream);
+                U6 = StreamUtil.ReadInt16(stream);
 
                 Padding = new List<int>();
                 for (int i = 0; i < PaddingCount; i++)
@@ -59,10 +84,11 @@ namespace SSXMultiTool.FileHandlers
 
         struct FileHeader
         {
-            public int Unknown1;
-            public int Offset;
+            public int Speaker;
+            public byte[] Offset;
             public int Unknown2;
-            public int Unknown3;
+
+            public int OffsetInt;
         }
     }
 }
