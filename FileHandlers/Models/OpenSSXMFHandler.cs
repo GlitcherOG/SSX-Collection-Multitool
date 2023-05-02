6ee2196ae4cfcd6dd6d3d77ad5e28ad8963aacc5
GlitcherOG <lachlankalf@gmail.com>
2023-05-02 16:23:46 +1000

    Create OpenSSXMFHandler.cs

---
+++ FileHandlers/Models/OpenSSXMFHandler.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSXMultiTool.FileHandlers.Models
+{
+    public class OpenSSXMFHandler
+    {
+        string MagicName = "OSMF";
+        int FormatVersion;
+        int ModelHeaderCount;
+        int ModelDataOffset;
+
+        public List<ModelHeader> modelHeaders = new List<ModelHeader>();
+
+
+
+
+
+        public struct ModelHeader
+        {
+            public string ModelName;
+
+            public int ModelOffset;
+            public int ModelSize;
+            public int MaterialOffset;
+            public int BoneOffset;
+            public int IKOffset;
+            public int WeightOffset;
+            public int VertexOffset;
+            public int MorphOffset;
+            public int FaceOffset;
+
+            public int MaterialCount;
+            public int BoneCount;
+            public int IKCount;
+            public int WeightCount;
+            public int VertexCount;
+            public int MorphCount;
+            public int FaceCount;
+
+            public int FileID;
+
+            public List<MaterialData> materialDatas;
+            public List<BoneData> boneDatas;
+            public List<MorphHeader> morphDatas;
+            public List<BoneWeightHeader> boneWeightHeaders;
+            public List<VertexData> vertexDatas;
+            public List<Face> faces;
+        }
+
+        public struct MaterialData
+        {
+            public string MainTexture;
+            public string Texture1;
+            public string Texture2;
+            public string Texture3;
+            public string Texture4;
+
+            public float FactorFloat;
+            public float Unused1Float;
+            public float Unused2Float;
+        }
+
+        public struct BoneData
+        {
+            public string BoneName;
+            public int ParentFileID;
+            public int ParentBone;
+            public int BoneID;
+
+            public Vector3 Position;
+            public Vector3 Radians;
+
+            //Non Saving Data
+            public int FileID;
+            public int BonePos;
+
+            public string parentName;
+        }
+
+        public struct MorphHeader
+        {
+            public string MorphName; //Unknown if will keep this
+
+            public int NumMorphData;
+            public int OffsetMorphDataList;
+
+            public List<MorphData> MorphDataList;
+        }
+
+        public struct MorphData
+        {
+            public Vector3 Morph;
+            public int VertexIndex;
+        }
+
+        public struct BoneWeightHeader
+        {
+            public int WeightCount;
+            public int WeightListOffset;
+
+            public List<BoneWeight> boneWeights;
+        }
+
+        public struct BoneWeight
+        {
+            public int Weight;
+            public int BoneID;
+            public int FileID;
+
+            //Non Saving Data
+            public string BoneName;
+        }
+
+        public struct VertexData
+        {
+            public Vector3 VertexPosition;
+            public Vector3 VertexNormal;
+            public Vector2 VertexUV;
+            public int WeightIndex;
+
+            //Added Here From Morph
+            public List<Vector3> MorphData;
+        }
+
+        public struct Face
+        {
+            public int V1Pos;
+            public int V2Pos;
+            public int V3Pos;
+            public int MaterialID;
+        }
+    }
+
+
+
+}
