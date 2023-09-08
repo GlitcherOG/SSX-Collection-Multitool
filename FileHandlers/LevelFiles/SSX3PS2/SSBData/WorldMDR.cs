f47622a106b9a2181aa3f99f9813d10027c3056d
GlitcherOG <lachlankalf@gmail.com>
2023-09-08 23:54:55 +1000

    Full Model Reading

--- FileHandlers/LevelFiles/SSX3PS2/SSBData/WorldMDR.cs
+++ FileHandlers/LevelFiles/SSX3PS2/SSBData/WorldMDR.cs
@@ -102,18 +102,18 @@ namespace SSXMultiTool.FileHandlers.LevelFiles.SSX3PS2.SSBData
                         TempS5.unknownS6.U1 = StreamUtil.ReadInt16(stream);
                         TempS5.unknownS6.U3Offset = StreamUtil.ReadUInt32(stream);
 
-                        TempS5.unknownS6.unknownS7 = new List<UnknownS7>();
+                        TempS5.unknownS6.ModelOffsetHeaders = new List<ModelOffsetHeader>();
 
                         stream.Position = TempS5.unknownS6.U3Offset + U10Offset;
 
                         while (true)
                         {
-                            UnknownS7 TempS7 = new UnknownS7();
+                            ModelOffsetHeader TempS7 = new ModelOffsetHeader();
 
-                            TempS7.U0 = StreamUtil.ReadInt24(stream);
+                            TempS7.LineCount = StreamUtil.ReadInt24(stream);
                             TempS7.U1 = StreamUtil.ReadUInt8(stream);
-                            TempS7.U2 = StreamUtil.ReadInt24(stream);
-                            TempS5.unknownS6.unknownS7.Add(TempS7);
+                            TempS7.ModelOffset = StreamUtil.ReadInt24(stream);
+                            TempS5.unknownS6.ModelOffsetHeaders.Add(TempS7);
 
                             StreamUtil.AlignBy16(stream);
 
@@ -132,10 +132,26 @@ namespace SSXMultiTool.FileHandlers.LevelFiles.SSX3PS2.SSBData
                 if (TempS1.U2Offset > 0)
                 {
                     stream.Position = TempS1.U2Offset;
-                    UnknownS4 TempS4 = new UnknownS4();
-                    TempS4.U0 = StreamUtil.ReadUInt32(stream);
-                    TempS4.U1 = StreamUtil.ReadUInt32(stream);
-                    TempS1.unknownS4 = TempS4;
+                    UnknownS3 TempS3 = new UnknownS3();
+                    TempS3.U0 = StreamUtil.ReadVector3(stream);
+                    TempS3.U2 = StreamUtil.ReadVector3(stream);
+
+                    TempS3.U3 = StreamUtil.ReadUInt32(stream);
+                    TempS3.U4 = StreamUtil.ReadUInt32(stream);
+                    TempS3.U5 = StreamUtil.ReadUInt32(stream);
+                    TempS3.U6 = StreamUtil.ReadUInt32(stream);
+
+                    TempS3.U7 = StreamUtil.ReadUInt32(stream);
+                    TempS3.U8 = StreamUtil.ReadUInt32(stream);
+                    TempS3.U9 = StreamUtil.ReadUInt32(stream);
+                    TempS3.U10 = StreamUtil.ReadUInt32(stream);
+
+                    TempS3.U11 = StreamUtil.ReadUInt32(stream);
+                    TempS3.U12 = StreamUtil.ReadUInt32(stream);
+                    TempS3.U13 = StreamUtil.ReadUInt32(stream);
+                    TempS3.U14 = StreamUtil.ReadUInt32(stream);
+
+                    TempS1.unknownS3 = TempS3;
                 }
 
                 if (TempS1.MatrixOffset > 0)
@@ -150,6 +166,122 @@ namespace SSXMultiTool.FileHandlers.LevelFiles.SSX3PS2.SSBData
                 UnknownS1s.Add(TempS1);
             }
 
+
+            for (int i = 0; i < UnknownS1s.Count; i++)
+            {
+                var S1 = UnknownS1s[i];
+
+                if(S1.U1Offset>0)
+                {
+                    for (int a = 0; a < S1.unknownS2.unknownS5.Count; a++)
+                    {
+                        var S5 = S1.unknownS2.unknownS5[a];
+                        bool Normal = false;
+                        for (int b = 0; b < S5.unknownS6.ModelOffsetHeaders.Count; b++)
+                        {
+                            var ModelOffsetHeader = S5.unknownS6.ModelOffsetHeaders[b];
+
+                            stream.Position = ModelOffsetHeader.ModelOffset + U10Offset;
+                            if (b==0)
+                            {
+                                //Unknown if used
+                                stream.Position += 32;
+                            }
+
+                            if(!Normal)
+                            {
+                                stream.Position += 32;
+                                ModelVandUVData TempModelData = new ModelVandUVData();
+
+                                TempModelData.TristripCount = StreamUtil.ReadUInt32(stream);
+
+                                stream.Position += 4;
+
+                                TempModelData.VerticesCount = StreamUtil.ReadUInt32(stream);
+
+                                stream.Position += 4 + 16;
+
+                                TempModelData.Tristrip = new List<int>();
+
+                                for (int c = 0; c < TempModelData.TristripCount; c++)
+                                {
+                                    TempModelData.Tristrip.Add(StreamUtil.ReadUInt32(stream));
+                                    StreamUtil.AlignBy16(stream);
+                                }
+
+                                stream.Position += 16;
+
+                                TempModelData.Vertices = new List<Vector3>();
+
+                                for (int c = 0; c < TempModelData.VerticesCount; c++)
+                                {
+                                    Vector3 vector3 = new Vector3();
+
+                                    vector3.X = StreamUtil.ReadInt16(stream) / 1024f;
+                                    vector3.Y = StreamUtil.ReadInt16(stream) / 1024f;
+                                    vector3.Z = StreamUtil.ReadInt16(stream) / 1024f;
+
+                                    TempModelData.Vertices.Add(vector3);
+                                }
+                                StreamUtil.AlignBy16(stream);
+
+                                stream.Position += 16;
+                                TempModelData.UV = new List<Vector2>();
+
+                                for (int c = 0; c < TempModelData.VerticesCount; c++)
+                                {
+                                    Vector2 vector3 = new Vector2();
+
+                                    vector3.X = StreamUtil.ReadInt16(stream) / 1024f;
+                                    vector3.Y = StreamUtil.ReadInt16(stream) / 1024f;
+
+                                    TempModelData.UV.Add(vector3);
+                                }
+                                StreamUtil.AlignBy16(stream);
+
+                                ModelOffsetHeader.modelVandUVData = TempModelData;
+                            }
+                            else
+                            {
+                                ModelNormalData TempModelData = new ModelNormalData();
+
+                                stream.Position += 14;
+
+                                TempModelData.NormalCount = StreamUtil.ReadUInt8(stream);
+
+                                stream.Position += 1;
+
+                                TempModelData.Normals = new List<Vector3>();
+
+                                for (int c = 0; c < TempModelData.NormalCount; c++)
+                                {
+                                    Vector3 vector3 = new Vector3();
+
+                                    vector3.X = StreamUtil.ReadInt16(stream) / 1024f;
+                                    vector3.Y = StreamUtil.ReadInt16(stream) / 1024f;
+                                    vector3.Z = StreamUtil.ReadInt16(stream) / 1024f;
+
+                                    TempModelData.Normals.Add(vector3);
+                                }
+                                StreamUtil.AlignBy16(stream);
+
+                                ModelOffsetHeader.modelNormalData = TempModelData;
+                            }
+
+                            Normal = !Normal;
+
+                            S5.unknownS6.ModelOffsetHeaders[b] = ModelOffsetHeader;
+                        }
+
+                        S1.unknownS2.unknownS5[a] = S5;
+                    }
+                }
+
+                UnknownS1s[i] = S1;
+            }
+
+
+
             stream.Close();
         }
 
@@ -162,7 +294,7 @@ namespace SSXMultiTool.FileHandlers.LevelFiles.SSX3PS2.SSBData
 
             public Matrix4x4 matrix4X4;
             public UnknownS2 unknownS2;
-            public UnknownS4 unknownS4;
+            public UnknownS3 unknownS3;
         }
 
         public struct UnknownS2
@@ -197,12 +329,6 @@ namespace SSXMultiTool.FileHandlers.LevelFiles.SSX3PS2.SSBData
             public int U14;
         }
 
-        public struct UnknownS4
-        {
-            public int U0;
-            public int U1;
-        }
-
         public struct UnknownS5
         {
             public int Offset;
@@ -215,14 +341,34 @@ namespace SSXMultiTool.FileHandlers.LevelFiles.SSX3PS2.SSBData
             public int U1;
             public int U3Offset;
 
-            public List <UnknownS7> unknownS7;
+            public List <ModelOffsetHeader> ModelOffsetHeaders;
         }
 
-        public struct UnknownS7
+        public struct ModelOffsetHeader
         {
-            public int U0;
+            public int LineCount;
             public int U1;
-            public int U2;
+            public int ModelOffset;
+
+            public ModelVandUVData modelVandUVData;
+            public ModelNormalData modelNormalData;
+        }
+
+        public struct ModelVandUVData
+        {
+            public int TristripCount;
+            public int VerticesCount;
+
+            public List<int> Tristrip;
+            public List<Vector3> Vertices;
+            public List<Vector2> UV;
+        }
+
+        public struct ModelNormalData
+        {
+            public int NormalCount;
+
+            public List<Vector3> Normals;
         }
     }
 }
