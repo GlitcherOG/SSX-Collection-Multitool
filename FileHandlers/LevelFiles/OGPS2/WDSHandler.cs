3c5571831efc27f25b2e385d965fa01d6e3f6959
GlitcherOG <lachlankalf@gmail.com>
2023-10-04 23:58:35 +1100

    Update WDSHandler.cs

--- FileHandlers/LevelFiles/OGPS2/WDSHandler.cs
+++ FileHandlers/LevelFiles/OGPS2/WDSHandler.cs
@@ -8,7 +8,33 @@ namespace SSXMultiTool.FileHandlers.LevelFiles.OGPS2
 {
     public class WDSHandler
     {
-        //Links thinks this is logic
+        public float U0;
+        public float U1;
+        public float U2;
+        public float U3;
 
+        public float U4;
+        public int U5;
+        public int UStruct2Count;
+        public int RowCount;
+
+        public int CollumCount;
+        public int U9;
+        public int U10;
+        public int U11;
+
+        public string FilePath;
+        public byte[] Unused; //64 - file path lenght appears to be unused
+
+        public List<UStruct1> uStruct1s;
+
+        public struct UStruct1
+        {
+            public int ID;
+            public int Count;
+            public int StartPos;
+
+            public List<int> ints;
+        }
     }
 }
