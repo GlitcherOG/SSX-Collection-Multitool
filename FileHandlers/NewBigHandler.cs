e02adb007bc9178b2101450d47888ec599323cc6
GlitcherOG <lachlankalf@gmail.com>
2023-12-04 19:27:41 +1100

    2012 Big start

---
+++ FileHandlers/NewBigHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSXMultiTool.FileHandlers
+{
+    public class NewBigHandler
+    {
+//4 - header
+//4 - count
+//4 - unknown
+//4 - path offset
+//4 - data start from path offset
+//2 - unknown
+//2 - folder path count
+//4 - unknown
+//4 - size
+//16 - unknown
+
+//4 - unknown
+//4 - unknown
+//4 - size?
+//4 - unknown
+    }
+}
