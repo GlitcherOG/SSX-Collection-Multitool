81939891a750b5ef0983cbced59a43878ff2b92a
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2024-01-05 10:27:56 +1100

    Update VaultBinHandler.cs

--- FileHandlers/SSX2012/VaultBinHandler.cs
+++ FileHandlers/SSX2012/VaultBinHandler.cs
@@ -1,12 +1,79 @@
-﻿using System;
+﻿using SSXMultiTool.Utilities;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static SSXMultiTool.FileHandlers.SSX2012.InfoDLCHandler;
 
 namespace SSXMultiTool.FileHandlers.SSX2012
 {
-    internal class VaultBinHandler
+    public class VaultBinHandler
     {
+        public void Load(string path)
+        {
+            using (Stream stream = File.Open(path, FileMode.Open))
+            {
+                //Version magic 4 bytes
+                //4 bytes int size
+                //8 bytes
+
+                //StrN Magic 4 bytes
+                //4 bytes int size
+                //8 bytes blank
+
+                //DepN Magic 4 Bytes
+                //4 bytes int size
+                //4 bytes int
+                //8 bytes
+                //4 bytes int
+                //4 bytes int string size
+                //vault string
+                //bin string
+                //allign byte 16
+
+                //DatN magic 4 bytes
+                //4 bytes int size
+                //8 bytes
+                
+                //DatN Struct 128-375, 1616-1863, 1864-1983
+                //16 bytes header?
+
+
+
+
+
+
+
+
+                //ExpN magic 4 bytes
+                //4 bytes int size
+                //4 bytes int count
+
+                //ExpN Struct
+                //8 bytes
+                //4 bytes int
+                //4 bytes int
+
+                //Allign by 16
+
+                //PtrN Magic 4 Bytes
+                //4 bytes int size
+                //4 bytes
+
+                //PrtN Struct
+                //2 bytes int
+                //2 bytes int
+                //4 bytes int bin data offset?
+                //4 bytes int
+
+                //Allign by 16
+
+                //EndC Magic 4 Bytes
+                //4 bytes int size
+                //Allign by 16
+            }
+        }
     }
 }
