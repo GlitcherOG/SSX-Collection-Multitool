31eb3bb4820b30a19c00165f2a6b52c7a82a5c14
GlitcherOG <lachlankalf@gmail.com>
2024-02-03 16:08:18 +1100

    GEOM Handler 2012

---
+++ FileHandlers/Models/SSX2012/GEOMHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSXMultiTool.FileHandlers.Models.SSX2012
+{
+    internal class GEOMHandler
+    {
+
+    }
+}
+
+/*struct Vector
+{
+float X;
+float Y;
+float Z;
+
+u32 U0;
+
+u64 Normal;
+
+u16 XUV;
+u16 YUV;
+u32 U1;
+
+};
+
+
+#pragma endian big
+le u32 Magic @ $;
+le u32 U1 @ $;
+le u32 U2 @ $;
+le u32 U3 @ $;
+
+le u32 GeomMagic @ $;
+le u32 U5 @ $;
+u32 GeomSize @ $;
+u32 U6 @ $;
+u32 U7 @ $;
+
+u32 VFMTMagic @ $;
+u32 CharSize @ $;
+u8 Text[CharSize+1] @ $;
+u32 U8 @ $;
+
+u32 STRMMagic @ $;
+u8 U9 @ $;
+u32 U10 @ $;
+u32 NumVertices @ $;
+u32 U11 @ $;
+u32 U12 @ $;
+
+Vector Vectors[NumVertices] @ $;*/
