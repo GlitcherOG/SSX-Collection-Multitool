4a98cf4bf20d816a46b275988e769943428d3805
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2024-02-08 14:32:54 +1100

    Create CRSFHandler.cs

---
+++ FileHandlers/Models/SSX2012/CRSFHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSXMultiTool.FileHandlers.Models.SSX2012
+{
+    public class CRSFHandler
+    {
+        public string Magic;
+        public int U1;
+        public int U2;
+        public int U3;
+
+
+
+        public struct WEITStruct
+        {
+            public string WEITMagic;
+            public int U0;
+            public int U1;
+            public int U2;
+            public int U3;
+            public int U4;
+            public int U5;
+            public int U6;
+
+            public List<WeightStruct> weightStructs;
+
+        }
+
+        public struct WeightStruct
+        {
+            public List<float> WeightValue;
+            public List<int> BoneIndex;
+        }
+
+        public struct SkelStructure
+        {
+            public string SkelMagic;
+            public int U0;
+            public int U1;
+            public int U2;
+            public int U3;
+
+            public List<BoneData> boneDatas;
+        }
+
+        public struct BoneData
+        {
+            public int BoneNameSize;
+            public string BoneName;
+            public Matrix4x4 matrix;
+        }
+    }
+}
