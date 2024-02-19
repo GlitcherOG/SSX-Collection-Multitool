020eb70ccabab00dabbe1d7be0b2bae55250a564
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2024-02-19 22:10:03 +1100

    Loading Rig

--- FileHandlers/Models/SSX2012/CRSFHandler.cs
+++ FileHandlers/Models/SSX2012/CRSFHandler.cs
@@ -19,6 +19,7 @@ namespace SSXMultiTool.FileHandlers.Models.SSX2012
         public WEITStruct weitStruct = new WEITStruct();
         public SkelStruct skelStruct = new SkelStruct();
         public FPOFStruct fpofStruct = new FPOFStruct();
+        public SGRFStruct sgrfStruct = new SGRFStruct();
 
         public void Load(string path)
         {
@@ -124,7 +125,124 @@ namespace SSXMultiTool.FileHandlers.Models.SSX2012
 
                 if(Test == "SGRF")
                 {
+                    stream.Position -= 4;
+                    sgrfStruct = new SGRFStruct();
 
+                    sgrfStruct.Magic = StreamUtil.ReadString(stream, 4);
+                    sgrfStruct.U0 = StreamUtil.ReadUInt32(stream, true);
+                    sgrfStruct.Size = StreamUtil.ReadUInt32(stream, true);
+
+                    sgrfStruct.SGNodeGroup = new List<SGNode>();
+                    for (int i = 0; i < 1; i++)
+                    {
+                        var TempSGNode = new SGNode();
+
+                        TempSGNode.U0 = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.NodeSize = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.NodeType = StreamUtil.ReadString(stream, TempSGNode.NodeSize +1);
+                        TempSGNode.U1 = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.NameSize = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.Name = StreamUtil.ReadString(stream, TempSGNode.NameSize + 1);
+                        TempSGNode.U11 = StreamUtil.ReadUInt16(stream, true);
+                        TempSGNode.U2 = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.U3 = StreamUtil.ReadUInt32(stream, true);
+
+                        sgrfStruct.SGNodeGroup.Add(TempSGNode);
+                    }
+
+                    sgrfStruct.SGNodeTransform = new List<SGNode>();
+                    for (int i = 0; i < 2; i++)
+                    {
+                        var TempSGNode = new SGNode();
+
+                        TempSGNode.U0 = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.NodeSize = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.NodeType = StreamUtil.ReadString(stream, TempSGNode.NodeSize + 1);
+                        TempSGNode.U1 = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.NameSize = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.Name = StreamUtil.ReadString(stream, TempSGNode.NameSize + 1);
+                        TempSGNode.U11 = StreamUtil.ReadInt8(stream);
+                        TempSGNode.U2 = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.U3 = StreamUtil.ReadUInt32(stream, true);
+
+                        TempSGNode.U4 = StreamUtil.ReadUInt32(stream, true);
+
+                        TempSGNode.transformDatas = new List<TransformData>();
+
+                        for (int a = 0; a < TempSGNode.U4; a++)
+                        {
+                            var TempTransformData = new TransformData();
+
+                            TempTransformData.Type = StreamUtil.ReadUInt32(stream, true);
+                            TempTransformData.U0 = StreamUtil.ReadUInt32(stream, true);
+                            TempTransformData.TransformNameSize = StreamUtil.ReadUInt32(stream, true);
+                            TempTransformData.TransformName = StreamUtil.ReadString(stream, TempTransformData.TransformNameSize + 1);
+                            TempTransformData.X = StreamUtil.ReadFloat(stream, true);
+                            TempTransformData.Y = StreamUtil.ReadFloat(stream, true);
+                            TempTransformData.Z = StreamUtil.ReadFloat(stream, true);
+
+                            TempSGNode.transformDatas.Add(TempTransformData);
+                        }
+
+                        TempSGNode.U5 = StreamUtil.ReadInt8(stream);
+                        TempSGNode.U6 = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.U7 = StreamUtil.ReadUInt32(stream, true);
+
+                        TempSGNode.matrix = StreamUtil.ReadMatrix4x4(stream, true);
+
+                        sgrfStruct.SGNodeTransform.Add(TempSGNode);
+                    }
+
+                    sgrfStruct.SGNodeJoint = new List<SGNode>();
+                    for (int i = 0; i < 279; i++)
+                    {
+                        var TempSGNode = new SGNode();
+
+                        TempSGNode.U0 = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.NodeSize = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.NodeType = StreamUtil.ReadString(stream, TempSGNode.NodeSize + 1);
+                        TempSGNode.U1 = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.NameSize = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.Name = StreamUtil.ReadString(stream, TempSGNode.NameSize + 1);
+                        TempSGNode.U11 = StreamUtil.ReadInt8(stream);
+                        TempSGNode.U2 = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.U3 = StreamUtil.ReadUInt32(stream, true);
+
+                        TempSGNode.U4 = StreamUtil.ReadUInt32(stream, true);
+
+                        TempSGNode.transformDatas = new List<TransformData>();
+
+                        for (int a = 0; a < TempSGNode.U4; a++)
+                        {
+                            var TempTransformData = new TransformData();
+
+                            TempTransformData.Type = StreamUtil.ReadUInt32(stream, true);
+                            TempTransformData.U0 = StreamUtil.ReadUInt32(stream, true);
+                            TempTransformData.TransformNameSize = StreamUtil.ReadUInt32(stream, true);
+                            TempTransformData.TransformName = StreamUtil.ReadString(stream, TempTransformData.TransformNameSize + 1);
+                            TempTransformData.X = StreamUtil.ReadFloat(stream, true);
+                            TempTransformData.Y = StreamUtil.ReadFloat(stream, true);
+                            TempTransformData.Z = StreamUtil.ReadFloat(stream, true);
+
+                            TempSGNode.transformDatas.Add(TempTransformData);
+                        }
+
+                        TempSGNode.U5 = StreamUtil.ReadInt8(stream);
+                        TempSGNode.U6 = StreamUtil.ReadUInt32(stream, true);
+                        TempSGNode.U7 = StreamUtil.ReadUInt32(stream, true);
+
+                        TempSGNode.matrix = StreamUtil.ReadMatrix4x4(stream, true);
+
+                        TempSGNode.U8 = StreamUtil.ReadUInt32(stream, true);
+
+                        TempSGNode.matrix1 = StreamUtil.ReadMatrix4x4(stream, true);
+                        TempSGNode.matrix2 = StreamUtil.ReadMatrix4x4(stream, true);
+                        TempSGNode.matrix3 = StreamUtil.ReadMatrix4x4(stream, true);
+
+                        TempSGNode.U9 = StreamUtil.ReadUInt32(stream, true);
+
+                        sgrfStruct.SGNodeJoint.Add(TempSGNode);
+                    }
                 }
             }
         }
@@ -217,7 +335,8 @@ namespace SSXMultiTool.FileHandlers.Models.SSX2012
             public string NodeType;
             public int U1;
             public int NameSize;
-            public string Name; //3 for base, 2 for base
+            public string Name; 
+            public int U11; //2 for base, 1 for rest
             public int U2; 
             public int U3;
 
