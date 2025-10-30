7ad683f1d5383042641349151347106b91bc7c54
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-10-30 23:37:16 +1100

    Create ErrorManager.cs

---
+++ Utilities/ErrorManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSXMultiTool.Utilities
+{
+    public class ErrorManager
+    {
+        public static string ErrorMessage = "Null";
+    }
+}
