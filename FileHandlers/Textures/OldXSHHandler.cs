66cc5e6d90e251fb7440ca77f0333922834f5778
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-11-25 19:04:55 +1100

    XSH Images

--- FileHandlers/Textures/OldXSHHandler.cs
+++ FileHandlers/Textures/OldXSHHandler.cs
@@ -1,25 +1,142 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BCnEncoder.Decoder;
+﻿using BCnEncoder.Decoder;
 using BCnEncoder.Encoder;
+using SSXMultiTool.Utilities;
+using System.IO;
+using System.Text;
 
 namespace SSXMultiTool.FileHandlers.Textures
 {
     internal class OldXSHHandler
     {
+        public string MagicWord; //4
+        public int Size;
+        public int ImageCount; //Big 4
+        public string GVersion;
+        public List<XSHImage> xshImage = new List<XSHImage>();
+        public string group;
+        public string endingstring;
 
-        void Test()
+        public void LoadXSH(string path)
         {
-            BcDecoder bcDecoder = new BcDecoder();
-            //bcDecoder.DecodeRaw();
+            using (Stream stream = File.Open(path, FileMode.Open))
+            {
+                MagicWord = StreamUtil.ReadString(stream, 4);
 
-            BcEncoder bcEncoder = new BcEncoder();
-            //bcEncoder.EncodeToDds(PixelFormat.)
+                if (MagicWord == "SHPX")
+                {
+                    Size = StreamUtil.ReadUInt32(stream);
+
+                    ImageCount = StreamUtil.ReadUInt32(stream);
+
+                    GVersion = StreamUtil.ReadString(stream, 4);
+
+                    for (int i = 0; i < ImageCount; i++)
+                    {
+                        XSHImage tempImage = new XSHImage();
+
+                        tempImage.shortname = StreamUtil.ReadString(stream, 4);
+
+                        tempImage.offset = StreamUtil.ReadUInt32(stream);
+
+                        xshImage.Add(tempImage);
+                    }
+
+                    group = StreamUtil.ReadString(stream, 8);
+
+                    endingstring = StreamUtil.ReadString(stream, 8);
+
+                    StandardToBitmap(stream);
+                }
+            }
+        }
+
+        public void StandardToBitmap(Stream stream)
+        {
+            byte[] tempByte;
+
+            for (int i = 0; i < xshImage.Count; i++)
+            {
+                XSHImage tempImage = xshImage[i];
+                XSHImageHeader tempImageHeader = new XSHImageHeader();
+
+                stream.Position = tempImage.offset;
+
+                tempImageHeader.MatrixFormat = StreamUtil.ReadUInt8(stream);
+
+                tempImageHeader.Size = StreamUtil.ReadInt24(stream);
+
+                tempImageHeader.Width = StreamUtil.ReadInt16(stream);
+
+                tempImageHeader.Height = StreamUtil.ReadInt16(stream);
+
+                tempImageHeader.Xaxis = StreamUtil.ReadInt16(stream);
+
+                tempImageHeader.Yaxis = StreamUtil.ReadInt16(stream);
+
+                //Add Other Flags Later
+                tempImageHeader.LXPos = StreamUtil.ReadInt16(stream);
+
+                tempImageHeader.TYPos = StreamUtil.ReadInt16(stream);
+
+                tempImageHeader.Matrix = StreamUtil.ReadBytes(stream, tempImageHeader.Size-16);
+
+                BcDecoder bcDecoder = new BcDecoder();
+
+                //96 - BCnEncoder.Shared.CompressionFormat.Bc1
+                //97 - BCnEncoder.Shared.CompressionFormat.Bc2
+                //109 - Bgra 4444 https://github.com/bartlomiejduda/EA-Graphics-Manager/blob/c9aec00c005437ddbc2752001913e1e2f46840e7/src/EA_Image/ea_image_decoder.py#L289
+                //123 - https://github.com/bartlomiejduda/EA-Graphics-Manager/blob/c9aec00c005437ddbc2752001913e1e2f46840e7/src/EA_Image/ea_image_decoder.py#L334
+                //125 - BCnEncoder.Shared.CompressionFormat.Bgra
+
+                var Temp = bcDecoder.DecodeRaw(tempImageHeader.Matrix, tempImageHeader.Width, tempImageHeader.Height, BCnEncoder.Shared.CompressionFormat.);
+
+                tempImage.bitmap = new Bitmap(tempImageHeader.Width, tempImageHeader.Height);
+
+                int post = 0;
+
+                for (global::System.Int32 y = 0; y < tempImageHeader.Height; y++)
+                {
+                    for (global::System.Int32 x = 0; x < tempImageHeader.Width; x++)
+                    {
+                        tempImage.bitmap.SetPixel(x, y, Color.FromArgb(Temp[post].a,Temp[post].r, Temp[post].g, Temp[post].b));
+                        post++;
+                    }
+                }
+
+                tempImage.bitmap.Save("G:\\SSX\\TRicky Stuff\\SSX Tricky\\data\\textures\\crowd.png");
+            }
 
         }
 
+        public struct XSHImage
+        {
+            public int offset;
+            public int size;
+            public string shortname;
+            public string longname;
+            public List<XSHImageHeader> xshShapeHeader;
+
+            //Converted
+            public Bitmap bitmap;
+        }
+
+        public struct XSHImageHeader
+        {
+            public byte MatrixFormat;
+            public int Size;
+            public int Width;
+            public int Height;
+            public int Xaxis;
+            public int Yaxis;
+            public int LXPos;
+            public bool flag1;
+            public bool flag2;
+            public bool flag3;
+            public bool flag4;
+            public int TYPos;
+            public int Mipmaps; //Unit4
+
+            public byte[] Matrix;
+        }
     }
 }
--- Tools/AudioTools.cs
+++ Tools/AudioTools.cs
@@ -1,22 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.IO;
+﻿using System.IO;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using System.Diagnostics;
-using System.Media;
-using NAudio;
 using NAudio.Wave;
-using SSXMultiTool.FileHandlers;
-using SSXMultiTool.FileHandlers.LevelFiles.SSX3PS2;
 using SSXMultiTool.FileHandlers.Audio;
-using Microsoft.Toolkit.HighPerformance;
 
 namespace SSXMultiTool.Tools
 {
