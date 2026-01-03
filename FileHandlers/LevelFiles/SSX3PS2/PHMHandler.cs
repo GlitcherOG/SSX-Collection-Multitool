828c4d27af72b4b1a9d5d6c4dcfcc4e640daea6c
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2026-01-04 01:36:58 +1100

    PHM and PSM Link

--- FileHandlers/LevelFiles/SSX3PS2/PHMHandler.cs
+++ FileHandlers/LevelFiles/SSX3PS2/PHMHandler.cs
@@ -1,14 +1,80 @@
 ﻿using SSXMultiTool.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace SSXMultiTool.FileHandlers.LevelFiles.SSX3PS2
 {
-    internal class PHMHandler
+    public class PHMHandler
     {
+        public float U0;
+        public float U1;
+        public int NumArrays;
 
+        public List<ResourceLink> resourceLinks = new List<ResourceLink>();
+        
+        public void LoadPHM(string path)
+        {
+            using (Stream stream = File.Open(path, FileMode.Open))
+            {
+                bool test = false;
+
+                U0 = StreamUtil.ReadFloat(stream);
+                U1 = StreamUtil.ReadFloat(stream);
+                NumArrays = StreamUtil.ReadUInt32(stream);
+                resourceLinks = new List<ResourceLink>();
+                for (int i = 0; i < NumArrays; i++)
+                {
+                    var TempEntryList = new ResourceLink();
+                    TempEntryList.U0 = StreamUtil.ReadUInt32(stream);
+                    TempEntryList.NumEntries = StreamUtil.ReadUInt32(stream);
+                    TempEntryList.Entries = new List<Entry>();
+                    for (int a = 0; a < TempEntryList.NumEntries; a++)
+                    {
+                        var TempEntry = new Entry();
+
+                        TempEntry.U0 = StreamUtil.ReadUInt32(stream);
+                        TempEntry.U1 = StreamUtil.ReadUInt32(stream);
+                        TempEntry.TrackID = StreamUtil.ReadInt8(stream);
+                        TempEntry.RID = StreamUtil.ReadUInt24(stream);
+                        TempEntry.U3 = StreamUtil.ReadUInt32(stream);
+
+                        TempEntryList.Entries.Add(TempEntry);
+                    }
+                    resourceLinks.Add(TempEntryList);
+                }
+            }
+        }
+
+        //0 - Patches
+        //1 - Models?
+        public string GetName(int TrackID, int RID, int ArrayID, PSMHandler psmHandler)
+        {
+            var Array = resourceLinks[ArrayID];
+
+            for (int i = 0; i < Array.Entries.Count; i++)
+            {
+                if (Array.Entries[i].RID == RID && Array.Entries[i].TrackID == TrackID)
+                {
+                    return psmHandler.nameLists[ArrayID].strings[i];
+                }
+            }
+
+            return "Null";
+        }
+
+        public struct ResourceLink
+        {
+            public int U0;
+            public int NumEntries;
+            public List<Entry> Entries;
+        }
+
+        public struct Entry
+        {
+            public int U0;
+            public int U1;
+            public int TrackID;
+            public int RID;
+            public int U3;
+        }
     }
 }
--- FileHandlers/LevelFiles/SSX3PS2/PSMHandler.cs
+++ FileHandlers/LevelFiles/SSX3PS2/PSMHandler.cs
@@ -1,16 +1,12 @@
 ﻿using SSXMultiTool.Utilities;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SSXMultiTool.FileHandlers.LevelFiles.SSX3PS2
 {
-    internal class PSMHandler
+    public class PSMHandler
     {
-        public byte[] Unknown1 = new byte[8];
+        public float U0;
+        public float U1;
         public int NumArrays;
         public List<NameList> nameLists = new List<NameList>();
 
@@ -20,19 +16,20 @@ namespace SSXMultiTool.FileHandlers.LevelFiles.SSX3PS2
             {
                 bool test = false;
 
-                Unknown1 = StreamUtil.ReadBytes(stream, 8);
+                U0  = StreamUtil.ReadFloat(stream);
+                U1 = StreamUtil.ReadFloat(stream);
                 NumArrays = StreamUtil.ReadUInt32(stream);
                 nameLists = new List<NameList>();
                 for (int i = 0; i < NumArrays; i++)
                 {
                     var TempNameList = new NameList();
-                    TempNameList.Unknown2 = StreamUtil.ReadBytes(stream, 4);
+                    TempNameList.Unknown2 = StreamUtil.ReadUInt32(stream);
                     TempNameList.NumStrings = StreamUtil.ReadUInt32(stream);
                     TempNameList.strings = new List<string>();
                     for (int a = 0; a < TempNameList.NumStrings; a++)
                     {
                         TempNameList.strings.Add(StreamUtil.ReadNullEndString(stream));
-                        stream.Position++;
+                        stream.Position++; //Put here to fix bug with Read Null End String
                     }
                     stream.Position++;
                     if(test)
@@ -45,10 +42,9 @@ namespace SSXMultiTool.FileHandlers.LevelFiles.SSX3PS2
             }
         }
 
-
         public struct NameList
         {
-            public byte[] Unknown2;
+            public int Unknown2;
             public int NumStrings;
             public List<string> strings;
         }
